b849d351d959bba29a3ffda49309cef307edf733
tanjahelen <cv57mhmscd@privaterelay.appleid.com>
2026-08-24 10:18:24 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="flokk-v2";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./styles.css","./app.js","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
